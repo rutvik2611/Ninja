42f9de4be46546b3608cbb3ecef1ce6e5928be6c
rutvik2611 <rutvik2611@gmail.com>
2023-10-15 18:35:30 -0400

    major changes
    Signed-off-by: rutvik2611 <rutvik2611@gmail.com>

--- src/services/cutcut/cutcut.py
+++ src/services/cutcut/cutcut.py
@@ -33,7 +33,7 @@ output_videos = []
 end_times = [sum(durations[:i]) for i in range(1, len(durations) + 1)]
 
 # Set the output directory
-output_directory = "/app/output_directory"  # You can change this path
+output_directory = "/app/input/output_directory"  # You can change this path
 
 # Cut the video based on the end times
 for i, end_time in enumerate(end_times):
