48550164b4bfa6c359470b1a6797c03ffd47481d
rutvik2611 <rutvik2611@gmail.com>
2023-12-09 14:15:37 -0500

    Update run.sh to bring Docker Compose files up and down in parallel

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Bring all Docker Compose files down in parallel
 for file in $(pwd)/src/docker-compose-files/*.yml
 do
-  # docker-compose -f $file down --remove-orphans
+  docker-compose -f $file down --remove-orphans &
+done
+
+# Wait for all background jobs to finish
+wait
+
+# Bring all Docker Compose files up in parallel
+for file in $(pwd)/src/docker-compose-files/*.yml
+do
   docker-compose -f $file up -d &
 done
 
