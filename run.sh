a00d0404d089d1296c78fe7e574304a0ed19568d
rutvik2611 <rutvik2611@gmail.com>
2023-12-08 19:38:10 -0500

    Refactor container startup logic in run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-echo "Starting to check for non-running or never started containers..."
-
 for file in $(pwd)/src/docker-compose-files/*.yml
 do
-  service=$(basename $file .yml)
-  echo "Checking service: $service"
-  
-  if [ "$(docker ps -a -q -f 'status=created' -f 'status=paused' -f 'status=restarting' -f 'status=removing' -f 'status=exited' -f 'status=dead' -f name=$service)" ]; then
-    echo "Found non-running containers for service: $service. Starting them..."
-    docker-compose -f $file up -d &
-  elif [ -z "$(docker ps -a -q -f name=$service)" ]; then
-    echo "No containers found for service: $service. Starting it..."
-    docker-compose -f $file up -d &
-  else
-    echo "No non-running containers found for service: $service"
-  fi
+  # docker-compose -f $file down --remove-orphans
+  docker-compose -f $file up -d &
 done
 
-echo "Finished checking for non-running or never started containers."
+# Wait for all background jobs to finish
+wait
