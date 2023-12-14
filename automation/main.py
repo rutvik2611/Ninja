d9bcb5c1251762e0b67a654da505613f5ca62976
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 16:03:49 -0500

    Add secure ID parameter to login form

--- automation/main.py
+++ automation/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+from helper.helper import get_page_source
 from login_automation import LoginAutomation
 from dotenv import load_dotenv
 import os
@@ -9,7 +11,7 @@ load_dotenv()
 # Load username and password from environment variables
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
-
+secure_id = os.getenv('SECURE_ID')
 # Create a new instance of the Chrome driver and keep it open after the script finishes
 options = Options()
 options.add_experimental_option("detach", True)
@@ -18,7 +20,18 @@ driver = webdriver.Chrome(options=options)
 # Create an instance of the LoginAutomation class
 login_automation = LoginAutomation(driver)
 
+# Save the HTML source of the current page to a file
+# get_page_source(driver, 'homepage')
+
 # Use the methods of the LoginAutomation class to automate the login process
-login_automation.navigate_to_website("https://myworkspace-car-1.jpmchase.com/logon/LogonPoint/tmindex.html")
+login_automation.navigate_to_website("https://myworkspace-cdc2-4.jpmchase.com/logon/LogonPoint/tmindex.html")
 login_automation.click_login_page_button("warnButton")
-login_automation.fill_login_form(username, password)
+# Save the HTML source of the redirected page to a file
+get_page_source(driver, 'after_click')
+login_automation.fill_login_form(username, password, secure_id)
+
+# Click the login button
+login_automation.click_login_button()
+
+# Save the HTML source of the page after login to a file
+get_page_source(driver, 'after_login')
