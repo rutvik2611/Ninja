4cdd9936d050a5b1426a0cc0eb8a1b5d617b080d
rutvik2611 <rutvik2611@gmail.com>
2023-12-15 15:01:31 -0500

    Update attempt status and add optional message parameter

--- optimus_db/secure_rsa_db/update_status.py
+++ optimus_db/secure_rsa_db/update_status.py
@@ -3,14 +3,14 @@ from sqlalchemy import update
 from optimus_db.db_connect import create_session_with_engine
 from optimus_db.secure_rsa_db.secure_rsa_db import secure_rsa
 
-def update_attempt_status(new_status):
+def update_attempt_status(new_status, msg=None):
     try:
         # Convert HTML to text
         with create_session_with_engine() as session:
 
             # Prepare the update statement
             stmt = update(secure_rsa).where(secure_rsa.c.valid == True).values(
-                attempt_status=new_status,
+                attempt_status=new_status,message=msg
             )
 
             # Execute the update statement
