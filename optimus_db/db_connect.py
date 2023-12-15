313e3556140272c050a0f056a05d5133c05dbbc0
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 19:55:37 -0500

    Refactor database connection code

--- optimus_db/db_connect.py
+++ optimus_db/db_connect.py
@@ -1,23 +1,20 @@
+import os
+from contextlib import contextmanager
 from dotenv import load_dotenv
-from sqlalchemy import text, create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import DBAPIError
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-from sqlalchemy.exc import DBAPIError
-from contextlib import contextmanager
-import os
-
-
-
-def c_engine():
+def create_sqlalchemy_engine():
     """Create a new SQLAlchemy engine."""
-    db = os.environ["DATABASE_URL"]
-    engine = create_engine(db, echo=True)
+    db_url = os.getenv("DATABASE_URL").replace("postgresql://", "cockroachdb://")
+    engine = create_engine(db_url, echo=True)
     return engine
 
 @contextmanager
-def db_session(engine):
+def create_db_session(engine):
     """Provide a transactional scope around a series of operations."""
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -31,10 +28,10 @@ def db_session(engine):
     finally:
         session.close()
 
-# Use the session in a context manager
-# Create engine
-# engine = c_engine()
-# with db_session(engine) as session:
-#     result = session.execute(text("SELECT now()"))
-#     for row in result:
-#         print(row)
+@contextmanager
+def create_session_with_engine():
+    """Create a new SQLAlchemy session with its own engine."""
+    engine = create_sqlalchemy_engine()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
