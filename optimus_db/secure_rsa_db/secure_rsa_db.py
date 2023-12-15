2ea5aa884a0ea2ec3645a5f00da2caf5aa74cdae
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 21:49:40 -0500

    Add columns to secure_rsa table

--- optimus_db/secure_rsa_db/secure_rsa_db.py
+++ optimus_db/secure_rsa_db/secure_rsa_db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import MetaData, Table, Column, PrimaryKeyConstraint, Integer, DateTime, func
+from sqlalchemy import Time,Date,MetaData, Table, Column, PrimaryKeyConstraint, Integer, DateTime, func, Boolean
 from optimus_db.db_connect import create_sqlalchemy_engine
 
 # Create engine
@@ -7,12 +7,14 @@ engine = create_sqlalchemy_engine()
 # Create a MetaData instance
 metadata = MetaData()
 
-# Create a table
 secure_rsa = Table(
    'secure_rsa', metadata,
    Column('id', Integer, primary_key=True, autoincrement=True),
    Column('rsa_value', Integer),
-   Column('time', DateTime, default=func.now(), onupdate=func.now())
+   Column('valid', Boolean,default=True),
+   Column('date', Date, default=func.current_date()),
+   Column('time', Time, default=func.current_time()),
+   Column('datetime', DateTime, default=func.now(), onupdate=func.now())
 )
 
 # Create all tables in the metadata
