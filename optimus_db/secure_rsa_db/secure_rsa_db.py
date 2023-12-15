d567bf9e58812649c81d44f3bb2579d5ccac563c
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 21:27:41 -0500

    Add secure RSA database functionality

---
+++ optimus_db/secure_rsa_db/secure_rsa_db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import MetaData, Table, Column, String, PrimaryKeyConstraint
+from optimus_db.db_connect import create_sqlalchemy_engine
+
+# Create engine
+engine = create_sqlalchemy_engine()
+
+# Create a MetaData instance
+metadata = MetaData()
+
+# Create a table
+env_variables = Table(
+   'env_variables', metadata, 
+   Column('ninja_key', String), 
+   Column('ninja_value', String), 
+   PrimaryKeyConstraint('ninja_key', name='ninja_key')
+)
+
+# Create all tables in the metadata
+metadata.create_all(engine)
