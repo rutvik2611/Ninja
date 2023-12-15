f203346a87181662d20cef61520219dbb2bfec37
rutvik2611 <rutvik2611@gmail.com>
2023-12-15 02:28:37 -0500

    Update attempt status and HTML content handling

--- optimus_db/secure_rsa_db/update_status.py
+++ optimus_db/secure_rsa_db/update_status.py
@@ -8,14 +8,10 @@ def update_attempt_status_and_html(new_status):
     try:
         # Convert HTML to text
         with create_session_with_engine() as session:
-            soup = BeautifulSoup(html_content, 'html.parser')
-            text_content = soup.get_text()
-            print(f'THIS  :{text_content}')
 
             # Prepare the update statement
             stmt = update(secure_rsa).where(secure_rsa.c.valid == True).values(
                 attempt_status=new_status,
-                html_content=text_content
             )
 
             # Execute the update statement
@@ -26,4 +22,4 @@ def update_attempt_status_and_html(new_status):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    update_attempt_status_and_html(True, '''<html><head><title>Test</title></head><body><p>Test</p></body></html>''')
+    update_attempt_status_and_html("failure")
