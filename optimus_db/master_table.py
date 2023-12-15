65463bb760acbdc09782de5ff31babcedcff0806
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 19:55:52 -0500

    Refactor master_table.py to use Ninja_ENV table

--- optimus_db/master_table.py
+++ optimus_db/master_table.py
@@ -1,31 +1,21 @@
-from sqlalchemy import Table, Column, String, MetaData
-from optimus_db.db_connect import db_session, c_engine
+from sqlalchemy import MetaData, Table, Column, String, PrimaryKeyConstraint
+from db_connect import create_sqlalchemy_engine, create_session_with_engine
 
 metadata = MetaData()
 
-rp = Table(
-   'rp', metadata,
-   Column('user', String, primary_key=True),
-   Column('secure_id', String),
-   comment="This table holds users and their secure ids"
+# Define the Ninja_ENV table
+Ninja_ENV = Table(
+   'Ninja_ENV', metadata,
+   Column('ninja_key', String),
+   Column('ninja_value', String),
+   PrimaryKeyConstraint('ninja_key', name='ninja_key')
 )
 
-engine = c_engine()
-with db_session(engine) as session:
-    metadata.create_all(engine)
-
-
-
-# Data to insert
-data = [
-    {"user": "user1", "secure_id": "id1"},
-    {"user": "user2", "secure_id": "id2"},
-    {"user": "user3", "secure_id": "id3"},
-    {"user": "user4", "secure_id": "id4"},
-    {"user": "user5", "secure_id": "id5"},
-]
+# Create the table in the database
+engine = create_sqlalchemy_engine()
+metadata.create_all(engine)
 
 # Use the session in a context manager
-with db_session(engine) as session:
-    # Insert the data
-    session.execute(rp.insert(), data)
+with create_session_with_engine() as session:
+    # You can now use the session to interact with the Ninja_ENV table
+    pass
