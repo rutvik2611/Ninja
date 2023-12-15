b19ead480142e60d01a713292aba874172d547fe
rutvik2611 <rutvik2611@gmail.com>
2023-12-14 21:49:35 -0500

    Refactor secure_rsa_db module to add new secure_rsa row
    This commit refactors the secure_rsa_db module to add a new function `add_secure_rsa` that adds a new row to the `secure_rsa` table. The function also sets the previous valid row to invalid. This change improves the functionality of the module and allows for better management of secure_rsa values.

--- optimus_db/secure_rsa_db/insert_secure_rsa.py
+++ optimus_db/secure_rsa_db/insert_secure_rsa.py
@@ -4,7 +4,7 @@ from optimus_db.db_connect import create_session_with_engine
 from optimus_db.secure_rsa_db.secure_rsa_db import secure_rsa
 
 
-def insert_secure_rsa(rsa_value):
+def insert_secure_rsa_1(rsa_value):
     """Insert a value into the secure_rsa table."""
     # Create a session
     with create_session_with_engine() as session:
@@ -18,5 +18,27 @@ def insert_secure_rsa(rsa_value):
             print(f"An error occurred while inserting value in rsa at python level: {e}")
             session.rollback()
 
+def add_secure_rsa(rsa_value):
+    """Add a new secure_rsa row and set the previous valid row to invalid."""
+    # Create a session
+    with create_session_with_engine() as session:
+        try:
+            # Find the current valid row and set it to invalid
+            session.query(secure_rsa).filter_by(valid=True).update({secure_rsa.c.valid: False})
+
+            # Add a new row with the given rsa_value and set it to valid
+            session.execute(secure_rsa.insert().values(rsa_value=rsa_value, valid=True))
+
+            # Commit the transaction
+            session.commit()
+        except Exception as e:
+            # Roll back the transaction
+            session.rollback()
+            print(f"An error occurred: {e}")
+        finally:
+            # Close the session
+            session.close()
+
 if __name__ == "__main__":
-    insert_secure_rsa("12343000")
+    # insert_secure_rsa("12343000")
+    add_secure_rsa("86510496")
