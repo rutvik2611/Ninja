a9beee3b75bf5800bc183f3198ef868a33c6650c
rutvik2611 <rutvik2611@gmail.com>
2023-12-18 16:08:15 -0500

    Refactor execute_sql.py to use SQLAlchemy's MetaData and Table classes for table operations

--- optimus_db/execute_sql.py
+++ optimus_db/execute_sql.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, MetaData, Table
 from db_connect import create_sqlalchemy_engine, create_db_session
 
 def execute_sql(sql_statement):
@@ -6,32 +6,46 @@ def execute_sql(sql_statement):
     # Execute the SQL statement
     with create_db_session() as session:
         result = session.execute(text(sql_statement))
-        session.commit()  # Commit the transaction
         return result
 
 def list_all_tables():
-    # SQL statement to get all table names
-    sql_statement = '''
-    SELECT table_name 
-    FROM information_schema.tables 
-    WHERE table_schema = 'public';
-    '''
-    result = execute_sql(sql_statement)
-    return result
+    with create_db_session() as session:
+        # Create a MetaData instance
+        meta = MetaData()
+
+        # Reflect the tables
+        meta.reflect(bind=session.get_bind())
+
+        # Get the table names
+        table_names = meta.tables.keys()
+        print(f'Tables in your db:\n{list(table_names)}')
+
+
+    return list(table_names)
 
 def ask_and_drop_table(table_name):
     # Ask the user if they want to drop the table
     answer = input(f"Do you want to drop the table '{table_name}'? (y/n) ")
     if answer.lower() == 'y':
-        # SQL statement to drop the table
-        sql_statement = f'DROP TABLE "{table_name}";'
-        execute_sql(sql_statement)
-        print(f"Table '{table_name}' dropped.")
+        with create_db_session() as session:
+            # Create a MetaData instance
+            meta = MetaData()
+
+            # Reflect the tables
+            meta.reflect(bind=session.get_bind())
+
+            # If the table exists
+            if table_name in meta.tables:
+                # Get the table
+                table = Table(table_name, meta)
+
+                # Drop the table
+                table.drop(bind=session.get_bind())
 
 # Example usage:
 if __name__ == "__main__":
     # List all tables
     result = list_all_tables()
     for row in result:
-        table_name = row[0]
+        table_name = row
         ask_and_drop_table(table_name)
