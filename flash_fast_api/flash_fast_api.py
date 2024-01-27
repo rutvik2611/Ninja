d54bbb787ce2ee02c9771c3b57872ed32a835032
rutvik2611 <rutvik2611@gmail.com>
2024-01-27 17:00:30 -0500

    Add new endpoint for fetching RSA value from PostgreSQL database

--- flash_fast_api/flash_fast_api.py
+++ flash_fast_api/flash_fast_api.py
@@ -15,6 +15,8 @@ from optimus_db.secure_rsa_db.fetch_latest_rsa import fetch_valid_rsa_value
 from optimus_db.secure_rsa_db.insert_secure_rsa import add_secure_rsa
 from optimus_db.secure_rsa_db.update_status import update_attempt_status
 
+from optimus_db.r743189.r743189_db import fetch_valid_rsa_postgres
+
 app = FastAPI()
 
 @app.get("/rsa")
@@ -54,3 +56,27 @@ def update_status(status: str):
         return {"message": "Status updated successfully"}
     except Exception as e:
         return {"error": str(e)}
+
+@app.get("/auto")
+def post_rsa2():
+    try:
+        rsa_value = fetch_valid_rsa_postgres()
+        if rsa_value is not None:
+            try:
+                with ThreadPoolExecutor() as executor:
+
+
+                    future1 = executor.submit(trigger, secure_id=rsa_value)
+                    future2 = executor.submit(add_secure_rsa, rsa_value)
+                    # Get the results of the tasks to raise any exceptions
+                    future1.result()
+                    future2.result()
+                update_attempt_status(new_status="success")
+                comment ="RSA value added successfully"
+            except Exception as e:
+                print(f"An error occurred in post_rsa: {e}")
+                update_attempt_status(new_status="failure",msg=str(e))
+                comment = "RSA value added successfully, but trigger failed: " + str(e)
+        return {"message": comment}
+    except Exception as e:
+        return {f"RSA:{rsa_value} error": str(e)}
