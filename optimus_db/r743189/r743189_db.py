23c186bef2e9aa0f378ee40bfb86041ee7dab8fd
rutvik2611 <rutvik2611@gmail.com>
2024-01-27 17:16:42 -0500

    Update database connection and session handling

--- optimus_db/r743189/r743189_db.py
+++ optimus_db/r743189/r743189_db.py
@@ -1,6 +1,20 @@
-from sqlalchemy import MetaData, Table, Column, String, Integer, DateTime, func, insert, update
-from optimus_db.db_connect import create_db_session
+from dotenv import load_dotenv
+import os
 
+from sqlalchemy import MetaData, Table, Column, String, Integer, DateTime, func, insert, update, create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Load environment variables from .env file
+load_dotenv()
+
+db_url = os.getenv("postgresql_connection")
+
+
+# create a configured "Session" class
+Session = sessionmaker(bind=create_engine(db_url, echo=True))
+
+# create a Session
+session = Session()
 
 # Create a MetaData instance
 metadata = MetaData()
@@ -14,8 +28,9 @@ r743189_table = Table(
 )
 
 
+
 def insert_rsa_value(rsa_value):
-    with create_db_session() as session:
+    with Session() as session:
         # Check if a record already exists
         record = session.query(r743189_table).first()
 
@@ -30,7 +45,7 @@ def insert_rsa_value(rsa_value):
 
 
 def update_rsa_value(user, rsa_value):
-    with create_db_session() as session:
+    with Session() as session:
         # Attempt to update the record for the user
         update_statement = update(r743189_table).where(r743189_table.c.user == user).values(rsa=rsa_value, time=func.now())
         result = session.execute(update_statement)
@@ -41,9 +56,32 @@ def update_rsa_value(user, rsa_value):
         else:
             session.commit()
 
+def print_table():
+    with Session() as session:
+        # Fetch all records from the table
+        records = session.query(r743189_table).all()
+
+        # Print each record
+        for record in records:
+            print(f"User: {record.user}, RSA: {record.rsa}, Time: {record.time}")
+
+def fetch_valid_rsa_postgres():
+    """Fetch the rsa_value of the row where valid is True."""
+    with Session() as session:
+            record = session.query(r743189_table).first()
+            if record is None:
+                raise Exception("No RSA value found in the database.")
+            else:
+                return record.rsa
+
+
 if __name__ == "__main__":
 
-    with create_db_session() as session:
-        metadata.create_all(session.get_bind())
-        insert_rsa_value(12345678)
-        update_rsa_value('r743189', 87654321)
+
+    with Session() as session:
+    #     # metadata.create_all(session.get_bind())
+    #     # insert_rsa_value(12345678)
+    #     # update_rsa_value('r743189', 87654321)
+        print(fetch_valid_rsa_postgres())
+        print_table()
+    # postgres_session()
